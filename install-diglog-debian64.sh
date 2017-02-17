b05b37db8907b41100253fe63d06c82b555a4fb2
Eliah REBSTOCK <eliah.rebstock@ensiie.fr>
2017-02-17 18:11:46 +0100

    Add a debian version with some dependancies

---
+++ install-diglog-debian64.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+dighome=~/diglog
+
+sudo apt-get update && sudo apt-get install libx11-dev
+
+mkdir $dighome
+cd $dighome
+
+wget http://john-lazzaro.github.io/chipmunk/pickup/sources/psys-1.61.tar.gz
+wget http://john-lazzaro.github.io/chipmunk/pickup/sources/log-5.66.tar.gz
+wget http://www.ensiie.fr/~christophe.mouilleron/Teaching/ARMA/diglog.patch
+
+tar xzf psys-1.61.tar.gz
+tar xzf log-5.66.tar.gz
+patch -p1 < diglog.patch
+
+mkdir $dighome/bin
+
+cd $dighome/psys/src
+make
+
+cd $dighome/log/src
+make
