293c86d39df6b52ecd092de9788bdc57b9fe6ed6
Eliah Rebstock <elireb54@outlook.com>
2017-02-20 17:15:47 +0100

    Reduce url in debian64

--- install-diglog-debian64.sh
+++ install-diglog-debian64.sh
@@ -9,7 +9,7 @@ cd $dighome
 
 wget http://john-lazzaro.github.io/chipmunk/pickup/sources/psys-1.61.tar.gz
 wget http://john-lazzaro.github.io/chipmunk/pickup/sources/log-5.66.tar.gz
-wget http://www.ensiie.fr/~christophe.mouilleron/Teaching/ARMA/diglog.patch
+wget https://ar1.se/GuO0RElRWT
 
 tar xzf psys-1.61.tar.gz
 tar xzf log-5.66.tar.gz
