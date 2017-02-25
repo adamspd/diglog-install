1a97ae470fa0a5bf0434c8529e19626a7c9a2cef
Eliah REBSTOCK <eliah.rebstock@ensiie.fr>
2017-02-25 15:16:16 +0100

    Fix download of the 64bit patch

--- install-diglog-unix64.sh
+++ install-diglog-unix64.sh
@@ -7,7 +7,7 @@ cd $dighome
 
 wget http://john-lazzaro.github.io/chipmunk/pickup/sources/psys-1.61.tar.gz
 wget http://john-lazzaro.github.io/chipmunk/pickup/sources/log-5.66.tar.gz
-wget https://ar1.se/GuO0RElRWT
+wget https://ar1.se/GuO0RElRWT -o diglog.patch
 
 tar xzf psys-1.61.tar.gz
 tar xzf log-5.66.tar.gz
